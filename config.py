209da10092a781e598b9103b076670e8bd145822
Patryk Balecki <patrykbalecki@Patryks-MBP.station>
2024-09-02 19:08:01 +0100

    chnaged database url to render postgres

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
 # When deploying, set in the environment to the PostgreSQL URL
-connection_string = 'postgresql://postgres:1212334@localhost:5432/postgres'
+#connection_string = 'postgresql://postgres:1212334@localhost:5432/postgres'
+connection_string = 'postgresql://patryk:snwVKN7q9CHzhJ7IXsuZQz5wJUU0ny3C@dpg-crauefdds78s73dacmdg-a.oregon-postgres.render.com/test_dx6x'
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', connection_string)
